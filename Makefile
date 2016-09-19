9ca3f2e0e6a8e7758c8f1be7515c3c8636cc4bb9
John Sieben <jsieben@cs4500.localdomain>
2016-09-19 17:39:30 -0600

    Cleanup leftover files

--- Makefile
+++
@@ -1,4 +0,0 @@
-##
-#Makefile of the new system call
-#
-obj-y := sys_helloworld.o sys_print_self.o sys_print_other.o
--- sys_helloworld.c
+++
@@ -1,7 +0,0 @@
-#include <linux/kernel.h>
-#include <linux/sched.h>
-asmlinkage int sys_helloworld (void)
-{
-  printk(KERN_EMERG "Hello World!\n");
-  return 1;
-}
